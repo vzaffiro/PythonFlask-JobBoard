0d75e4e05e9ab458df5884bd4b25599f9613cf0c
Vince Zaffiro <vincent.zaffiro@gmail.com>
2019-02-23 11:24:16 -0500

    Module 4

--- jobs/app.py
+++ jobs/app.py
@@ -34,4 +34,5 @@ def close_connection(exception):
 @app.route('/')
 @app.route('/jobs')
 def jobs():
-    return render_template('index.html')
+    jobs = execute_sql('select job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name from job JOIN employer on employer.id = job.employer_id')
+    return render_template('index.html', jobs=jobs)
